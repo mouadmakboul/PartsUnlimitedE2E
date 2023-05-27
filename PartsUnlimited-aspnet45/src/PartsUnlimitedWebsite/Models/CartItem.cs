304cf60df74fdd0e3624dffa06f97f9959f19f7e
Mouad Makboul <mouadmakboul8@gmail.com>
2023-05-27 15:08:08 +0100

    My commit

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
+    //My first change
     {
         [Key]
         public int CartItemId { get; set; }
